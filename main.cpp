4fe18ac01bac81639e317b1b63bf5391f77da511
teslaenergy@gmail.com <teslaenergy@gmail.com>
2021-03-13 20:37:47 -0700

    themes

--- main.cpp
+++ main.cpp
@@ -1,12 +1,22 @@
 #include "mainwindow.h"
 
 #include <QApplication>
-
+#include <QFile>
+#include <QSplashScreen>
+#include <QPixmap>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+
+    QFile file("media/themes/qdarkstyle/qdarkstyle.qss");
+    file.open(QFile::ReadOnly);
+    QString styleSheet = QLatin1String(file.readAll());
+
+    w.setStyleSheet(styleSheet);
+
+
     w.show();
     return a.exec();
 }
